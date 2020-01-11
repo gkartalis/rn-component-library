59fab561d5e6786ba45115102026ce819b0ea4ae
George Kartalis <g.kartalis7@gmail.com>
2020-01-11 16:11:46 +0200

    added @storybook/addon-storyshots and snapshot tests

---
+++ storybook/storyshots.test.js
@@ -0,0 +1,9 @@
+import initStoryShots from '@storybook/addon-storyshots';
+
+/*
+  This will automatically take all stories and
+  generate snapshot tests for all your stories
+  for free inside storybook/__snapshots__
+*/
+
+initStoryShots();
